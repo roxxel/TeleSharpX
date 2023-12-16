17795c5c09b3a7f6301ee786c872a963a66d7f5b
roxxel <roxxelroxx@gmail.com>
2023-12-16 23:06:05 +0200

    fix: lists wasn't correctly serialized

--- src/TeleSharpX/ApiClient.cs
+++ src/TeleSharpX/ApiClient.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections;
+using System.Linq;
 using System.Net.Http;
+using System.Reflection;
 using System.Text;
 using System.Text.Json;
 using Newtonsoft.Json;
@@ -15,7 +18,7 @@ namespace TeleSharpX
         private readonly string _token;
         private readonly HttpClient _client;
         public string _endpoint => $"https://api.telegram.org/bot{_token}/";
-        
+
         public ApiClient(string token)
         {
             _token = token;
@@ -23,40 +26,71 @@ namespace TeleSharpX
             _client.Timeout = TimeSpan.FromSeconds(30);
         }
 
+        public bool IsPropertyACollection(PropertyInfo property)
+        {
+            return (!typeof(String).Equals(property.PropertyType) && 
+                    typeof(IEnumerable).IsAssignableFrom(property.PropertyType));
+        }
+        
         public async Task<ApiResponse<T>> Send<T>(string methodName, HttpMethod method, object body)
         {
             var message = new HttpRequestMessage();
             message.RequestUri = new Uri($"{_endpoint}{methodName}");
             message.Method = method;
-
             if (method != HttpMethod.Get)
             {
-                MultipartFormDataContent form = new MultipartFormDataContent();
-                var fields = body.GetType().GetProperties();
-                foreach (var field in fields)
+                var containsFiles = body.GetType().GetProperties().Any(x => x.PropertyType == typeof(InputFile));
+                if (containsFiles)
                 {
-                    var name = field.Name;
-                    if (field.GetValue(body) == null) continue;
-                    if (field.PropertyType == typeof(InputFile))
+                    MultipartFormDataContent form = new MultipartFormDataContent();
+                    var fields = body.GetType().GetProperties();
+                    foreach (var field in fields)
                     {
-                        var value = field.GetValue(body) as InputFile;
-                        if (value.Type == InputFileType.Id)
+                        var name = field.Name;
+                        if (field.GetValue(body) == null) continue;
+                        if (field.PropertyType == typeof(InputFile))
                         {
-                            form.Add(new StringContent(value._id), name);
+                            var value = field.GetValue(body) as InputFile;
+                            if (value.Type == InputFileType.Id)
+                            {
+                                form.Add(new StringContent(value._id), name);
+                            }
+                            else if (value.Type == InputFileType.File)
+                            {
+                                form.Add(new ByteArrayContent(value._file, 0, value._file.Length), name, value._name);
+                            }
                         }
-                        else if (value.Type == InputFileType.File)
+                        else
                         {
-                            form.Add(new ByteArrayContent(value._file, 0, value._file.Length), name, value._name);
+                            if (IsPropertyACollection(field))
+                            {
+                                var js = JsonConvert.SerializeObject(field.GetValue(body), Formatting.None, new JsonSerializerSettings()
+                                {
+                                    NullValueHandling = NullValueHandling.Ignore
+                                });
+                                form.Add(new StringContent(js), name);
+                            }
+                            else
+                            {
+                                form.Add(new StringContent(field.GetValue(body).ToString()), name);
+                            }
                         }
                     }
-                    else
-                    {
-                        form.Add(new StringContent(field.GetValue(body).ToString()), name);
-                    }
-                }
 
-                message.Content = form;
+                    message.Content = form;
+                }
+                else
+                {
+                    var json = JsonConvert.SerializeObject(body, Formatting.None, new JsonSerializerSettings
+                    {
+                        NullValueHandling = NullValueHandling.Ignore
+                    });
+                    message.Content = new StringContent(json, Encoding.UTF8,
+                        "application/json");
+                    
+                }
             }
+
             var response = await _client.SendAsync(message);
             var respText = await response.Content.ReadAsStringAsync();
             var resp = JsonConvert.DeserializeObject<ApiResponse<T>>(respText);
